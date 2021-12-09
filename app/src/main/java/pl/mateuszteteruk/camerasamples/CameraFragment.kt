527674673ea23d90354441f53b4b936a51347f8a
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2021-12-09 09:04:48 +0100

    Add ImageCapture usecase and show taken picture as thumbnail in layout

--- app/src/main/java/pl/mateuszteteruk/camerasamples/CameraFragment.kt
+++ app/src/main/java/pl/mateuszteteruk/camerasamples/CameraFragment.kt
@@ -1,16 +1,24 @@
 package pl.mateuszteteruk.camerasamples
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.camera.core.AspectRatio
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import pl.mateuszteteruk.camerasamples.databinding.FragmentCameraBinding
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraFragment : Fragment() {
 
@@ -18,14 +26,23 @@ class CameraFragment : Fragment() {
     private val binding: FragmentCameraBinding
         get() = requireNotNull(_binding)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        cameraExecutor = Executors.newSingleThreadExecutor()
         startCamera()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        cameraExecutor.shutdown()
+        _binding = null
     }
 
     private fun startCamera() {
@@ -37,18 +54,52 @@ class CameraFragment : Fragment() {
                 .setTargetAspectRatio(AspectRatio.RATIO_16_9)
                 .build()
                 .apply {
-                    setSurfaceProvider(binding.previewFront.surfaceProvider)
+                    setSurfaceProvider(binding.cameraPreview.surfaceProvider)
                 }
 
-            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+            val imageCapture = ImageCapture.Builder()
+                .setCaptureMode(CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build()
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             try {
                 cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview)
+                cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview, imageCapture)
+                configureView(imageCapture)
             } catch (exception: Exception) {
-
+                Log.e("CameraFragment", "Camera exception: $exception")
             }
 
         }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun configureView(imageCapture: ImageCapture) {
+        binding.takePicture.setOnClickListener {
+
+            val outputOptions = ImageCapture.OutputFileOptions.Builder(
+                File.createTempFile("temp-file-${System.currentTimeMillis()}", "")
+            ).build()
+
+            imageCapture.takePicture(
+                outputOptions,
+                cameraExecutor,
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                        outputFileResults.savedUri?.let {
+                            binding.lastImagePreview.post {
+                                Glide.with(binding.lastImagePreview)
+                                    .load(it)
+                                    .into(binding.lastImagePreview)
+                            }
+                        }
+                    }
+
+                    override fun onError(exception: ImageCaptureException) {
+                        Log.e("CameraFragment", "Error while taking picture: $exception")
+                    }
+                }
+            )
+        }
     }
 }
